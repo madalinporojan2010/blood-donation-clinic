9ad88323f49d3a720f3313bb519bca447890a418
Madalin Marian Porojan <madalin.porojan2000@gmail.com>
2023-05-07 20:43:33 +0300

    Update TestPatient.java

--- src/test/java/service/jpa/mysql/TestPatient.java
+++ src/test/java/service/jpa/mysql/TestPatient.java
@@ -1,7 +1,6 @@
 package service.jpa.mysql;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -27,100 +26,101 @@ import application.utils.ResponseMessage;
 @DisplayName("Patient Service")
 @ExtendWith(MockitoExtension.class)
 public class TestPatient {
-    private PatientService patientService;
+        private PatientService patientService;
 
-    @Mock
-    private PatientRepositoryModels patientRepositoryModels;
+        @Mock
+        private PatientRepositoryModels patientRepositoryModels;
 
-    @Mock
-    private ScheduleRepositoryModels scheduleRepositoryModels;
+        @Mock
+        private ScheduleRepositoryModels scheduleRepositoryModels;
 
-    @Mock
-    private PatientObservable patientObservable;
+        @Mock
+        private PatientObservable patientObservable;
 
-    @Mock
-    private PatientObserver patientObserver;
+        @Mock
+        private PatientObserver patientObserver;
 
-    @BeforeEach
-    void setUp() {
-        this.patientService = new PatientService(patientRepositoryModels, scheduleRepositoryModels, patientObserver,
-                patientObservable);
-    }
+        @BeforeEach
+        void setUp() {
+                this.patientService = new PatientService(patientRepositoryModels, scheduleRepositoryModels,
+                                patientObserver,
+                                patientObservable);
+        }
 
-    @Test
-    @DisplayName("Ensure `findAllPatients` works")
-    void testFindAllPatient() {
-        List<Patient> datientsListMock = Mockito.mock();
-        when(patientRepositoryModels.findAll()).thenReturn(datientsListMock);
+        @Test
+        @DisplayName("Ensure `findAllPatients` works")
+        void testFindAllPatient() {
+                List<Patient> datientsListMock = Mockito.mock();
+                when(patientRepositoryModels.findAll()).thenReturn(datientsListMock);
 
-        assertEquals(datientsListMock, patientService.findAllPatients());
-        verify(patientRepositoryModels).findAll();
-    }
+                assertEquals(datientsListMock, patientService.findAllPatients());
+                verify(patientRepositoryModels).findAll();
+        }
 
-    @Test
-    @DisplayName("Ensure `savePatient` works")
-    void testSavePatient() {
-        Patient patient = Mockito.mock();
+        @Test
+        @DisplayName("Ensure `savePatient` works")
+        void testSavePatient() {
+                Patient patient = Mockito.mock();
 
-        // test for null blood type
-        when(patient.getBloodType()).thenReturn(null);
-        assertEquals(ResponseMessage.SUCCESS,
-                patientService.savePatient(patient).getMessage());
+                // test for null blood type
+                when(patient.getBloodType()).thenReturn(null);
+                assertEquals(ResponseMessage.SUCCESS,
+                                patientService.savePatient(patient).getMessage());
 
-        // test for not null blood type
-        when(patient.getBloodType()).thenReturn(Mockito.mock());
-        assertEquals(ResponseMessage.SUCCESS,
-                patientService.savePatient(patient).getMessage());
+                // test for not null blood type
+                when(patient.getBloodType()).thenReturn(Mockito.mock());
+                assertEquals(ResponseMessage.SUCCESS,
+                                patientService.savePatient(patient).getMessage());
 
-        verify(patientObservable, times(1)).addPatient(patient);
-        verify(patientRepositoryModels, times(2)).save(patient);
-    }
+                verify(patientObservable, times(1)).addPatient(patient);
+                verify(patientRepositoryModels, times(2)).save(patient);
+        }
 
-    @Test
-    @DisplayName("Ensure `updatePatient` works")
-    void testUpdatePatient() {
-        Patient patient = Mockito.mock();
+        @Test
+        @DisplayName("Ensure `updatePatient` works")
+        void testUpdatePatient() {
+                Patient patient = Mockito.mock();
 
-        // if patient exists in db
-        when(patientRepositoryModels.existsById(patient.getId())).thenReturn(true);
+                // if patient exists in db
+                when(patientRepositoryModels.existsById(patient.getId())).thenReturn(true);
 
-        // bloodType null or patientObservable patients null:
-        when(patient.getBloodType()).thenReturn(null);
-        assertEquals(ResponseMessage.SUCCESS,
-                patientService.updatePatient(patient).getMessage());
+                // bloodType null or patientObservable patients null:
+                when(patient.getBloodType()).thenReturn(null);
+                assertEquals(ResponseMessage.SUCCESS,
+                                patientService.updatePatient(patient).getMessage());
 
-        // bloodType not null and patientObservable patients not null:
-        when(patient.getBloodType()).thenReturn(Mockito.mock());
-        when(patientObservable.getPatients()).thenReturn(Mockito.mock());
-        assertEquals(ResponseMessage.SUCCESS,
-                patientService.updatePatient(patient).getMessage());
+                // bloodType not null and patientObservable patients not null:
+                when(patient.getBloodType()).thenReturn(Mockito.mock());
+                when(patientObservable.getPatients()).thenReturn(Mockito.mock());
+                assertEquals(ResponseMessage.SUCCESS,
+                                patientService.updatePatient(patient).getMessage());
 
-        // if patient doesnt exist in db
-        when(patientRepositoryModels.existsById(patient.getId())).thenReturn(false);
-        assertEquals(ResponseMessage.ERROR_ENTRY_NOT_PRESENT,
-                patientService.updatePatient(patient).getMessage());
+                // if patient doesnt exist in db
+                when(patientRepositoryModels.existsById(patient.getId())).thenReturn(false);
+                assertEquals(ResponseMessage.ERROR_ENTRY_NOT_PRESENT,
+                                patientService.updatePatient(patient).getMessage());
 
-        verify(patientRepositoryModels, times(3)).existsById(patient.getId());
-        verify(patientRepositoryModels, times(2)).save(patient);
-        verify(patient, times(2)).getBloodType();
+                verify(patientRepositoryModels, times(3)).existsById(patient.getId());
+                verify(patientRepositoryModels, times(2)).save(patient);
+                verify(patient, times(2)).getBloodType();
 
-    }
+        }
 
-    @Test
-    @DisplayName("Ensure `deletePatient` works")
-    void testDeletePatient() {
-        Patient patient = Mockito.mock();
+        @Test
+        @DisplayName("Ensure `deletePatient` works")
+        void testDeletePatient() {
+                Patient patient = Mockito.mock();
 
-        when(patientRepositoryModels.existsById(patient.getId())).thenReturn(true);
-        assertEquals(ResponseMessage.SUCCESS,
-                patientService.deletePatient(patient.getId()).getMessage());
+                when(patientRepositoryModels.existsById(patient.getId())).thenReturn(true);
+                assertEquals(ResponseMessage.SUCCESS,
+                                patientService.deletePatient(patient.getId()).getMessage());
 
-        when(patientRepositoryModels.existsById(patient.getId())).thenReturn(false);
-        assertEquals(ResponseMessage.ERROR_ENTRY_NOT_PRESENT,
-                patientService.deletePatient(patient.getId()).getMessage());
+                when(patientRepositoryModels.existsById(patient.getId())).thenReturn(false);
+                assertEquals(ResponseMessage.ERROR_ENTRY_NOT_PRESENT,
+                                patientService.deletePatient(patient.getId()).getMessage());
 
-        verify(patientRepositoryModels, times(2)).existsById(patient.getId());
-        verify(patientRepositoryModels, times(1)).deleteById(patient.getId());
-        verify(patientObservable).removePatient(patient.getId());
-    }
+                verify(patientRepositoryModels, times(2)).existsById(patient.getId());
+                verify(patientRepositoryModels, times(1)).deleteById(patient.getId());
+                verify(patientObservable).removePatient(patient.getId());
+        }
 }
